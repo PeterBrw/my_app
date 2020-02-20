11e0fe5186be1ce730306144ddf1a22b0d069577
PeterBrw <peter.giurgiu@gmail.com>
2020-02-20 14:52:53 +0200

    Updates

---
+++ precache-manifest.cb1f3a69ae5dfe22652c29ff8d432f12.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fcb667ab8ce9eef3c9d8b9a2c443e7a",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "5d61b8db635d6fcde601",
+    "url": "/my-app/static/css/main.a9827d45.chunk.css"
+  },
+  {
+    "revision": "0be154a3e0c083d1f41f",
+    "url": "/my-app/static/js/2.718ed629.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/my-app/static/js/2.718ed629.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d61b8db635d6fcde601",
+    "url": "/my-app/static/js/main.86701876.chunk.js"
+  },
+  {
+    "revision": "134c6d7b033e4b816dc9",
+    "url": "/my-app/static/js/runtime-main.56c001b7.js"
+  }
+]);
